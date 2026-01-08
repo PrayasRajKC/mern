e00c82163b57e09add1192fb0f600d90114f234e
PrayasRajKC <prayasrajkc33@gmail.com>
2026-01-08 16:45:51 +0545

    js changes

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
-const person = 'heroasdasd';
-console.log(person);
+//primitive data types
+//string
+//number
+//boolean
+//undefined
+//null
+
+//reference data types
+//array
+//object
+//function
+
+
+
+
+//assignment operator, arithemtic operator,  comparison operator, ternary operator,logical operator, if-else operator, switch-case loops
+
+
+const calc = 'add';
+const a    = 69;
+const b    = 96;
+
+switch(calc) 
+{
+    case 'add':
+        console.log(`The sum of given number is ${a+b}`);
+        break;
+    case 'subtract':
+        console.log(`The difference of the given number is ${a-b}`);
+        break;
+    case 'multiply':
+        console.log(`The multiplication of the given number is ${a*b}`);
+        break;
+    case 'divide':
+        console.log(`The division of the given number is ${a/b}`);
+        break;
+    default:
+    console.log('NaN');
+}
+
+
+
+
+
+
