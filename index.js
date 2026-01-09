e4622459a3f35ffb919a27a28a99a03b2f4f1d78
PrayasRajKC <prayasrajkc33@gmail.com>
2026-01-09 16:56:15 +0545

    js

--- index.js
+++ index.js
@@ -16,30 +16,13 @@
 //assignment operator, arithemtic operator,  comparison operator, ternary operator,logical operator, if-else operator, switch-case loops
 
 
-const calc = 'add';
-const a    = 69;
-const b    = 96;
 
-switch(calc) 
-{
-    case 'add':
-        console.log(`The sum of given number is ${a+b}`);
-        break;
-    case 'subtract':
-        console.log(`The difference of the given number is ${a-b}`);
-        break;
-    case 'multiply':
-        console.log(`The multiplication of the given number is ${a*b}`);
-        break;
-    case 'divide':
-        console.log(`The division of the given number is ${a/b}`);
-        break;
-    default:
-    console.log('NaN');
-}
+const persons = [11, 22, 33, 44];
 
 
-
-
-
-
+// persons.push(55, 66);
+// persons.pop();
+// persons.shift();
+// persons.unshift(9, 7);
+// console.log(persons);
+console.log(persons.concat(9, 7, 9).concat([3, 4, 5, 6]));
